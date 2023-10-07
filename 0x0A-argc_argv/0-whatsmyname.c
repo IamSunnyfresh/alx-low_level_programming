3f2af283605620fd49d03ec910877fc8d89c72d1
iamsunnyfresh <adieogbor@gmail.co>
2023-10-07 16:56:26 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-int _atoi(char *s);
-
-#endif
+#include <stdio.h>
+/**
+ * main - is a function that prints the name of the file
+ * @argc: is an argument counter for char
+ * @argv: is an argument value for char
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
